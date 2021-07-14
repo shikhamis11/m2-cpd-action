f49f9804f192b89bbafd54ca5411e3b36422badc
Shikha Mishra <shikhamishra@Shikhas-MacBook-Pro.local>
2021-07-14 12:54:20 +0530

    m2 cpd action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
-sh -c "bin/magento dev:tests:run static"
+sh -c "/m2/bin/magento dev:tests:run static"
 sh -c "cd /m2/dev/tests/static && /m2/vendor/bin/phpunit -c /m2/dev/tests/static/phpunit.phpcpd.xml $*"
