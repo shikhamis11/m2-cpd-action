140046b05c5034cf6bf07d1844856c635a4855af
Shikha Mishra <shikhamishra@cedcoss.com>
2021-07-17 23:12:59 +0530

    Updated extension path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/sh -l
-sh -c "/m2/bin/magento dev:tests:run static"
-sh -c "cd /m2/dev/tests/static && /m2/vendor/bin/phpunit -c /m2/dev/tests/static/phpunit.phpcpd.xml $*"
+sh -c "cd /m2/dev/tests/static && /m2/vendor/bin/phpunit -c /m2/dev/tests/static/phpunit.phpcpd.xml /m2/app/"
